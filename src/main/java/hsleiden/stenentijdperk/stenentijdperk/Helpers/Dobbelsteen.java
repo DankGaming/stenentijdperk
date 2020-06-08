0c809890a40d55888375bfe0f237e30f835b8bcc
Juan <juantombal@live.nl>
2020-06-08 15:08:00 +0200

    errors fixed

--- src/main/java/hsleiden/stenentijdperk/stenentijdperk/Helpers/Dobbelsteen.java
+++ src/main/java/hsleiden/stenentijdperk/stenentijdperk/Helpers/Dobbelsteen.java
@@ -14,16 +14,6 @@ public class Dobbelsteen {
         ogen = 0;
     }
 
-    public void werp(int aantal) {
-        for (int i = 0; i < aantal; i++) {
-            dobbelstenen[i].werp();
-        }
-        for (int i = aantal; i < 10; i++) {
-            dobbelstenen[i].reset();
-        }
-        berekenTotaal();
-    }
-
     public void worp() {
         ogen = (int) (Math.random() * 6) + 1;
     }
