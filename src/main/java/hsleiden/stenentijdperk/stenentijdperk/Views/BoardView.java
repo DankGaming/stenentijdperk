10548a0bf09d588d72b8d178c18f9ce205354581
Fabian Nievaart <s1123285@student.hsleiden.nl>
2020-06-08 15:03:29 +0200

    Create basic BoardView

--- src/main/java/hsleiden/stenentijdperk/stenentijdperk/Views/BoardView.java
+++ src/main/java/hsleiden/stenentijdperk/stenentijdperk/Views/BoardView.java
@@ -1,5 +1,42 @@
 package hsleiden.stenentijdperk.stenentijdperk.Views;
 
-public class BoardView {
+import hsleiden.stenentijdperk.stenentijdperk.Controllers.BoardController;
+import hsleiden.stenentijdperk.stenentijdperk.Models.BoardModel;
+import javafx.geometry.Insets;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 
+public class BoardView {
+	private BoardController controller;
+	private BoardModel model;
+	private GridPane view;
+
+	public BoardView() {
+		this.controller = new BoardController();
+		setupPane();
+	}
+
+	public GridPane setScene() {
+		return view;
+	}
+
+	private void setupPane() {
+		this.view = new GridPane();
+		this.view.setPadding(new Insets(10, 10, 10, 10));
+		this.view.setStyle("-fx-background-color: d0c9b5");
+		int amountOfRows = 50;
+		int amountOfColumns = 50;
+
+		for (int i = 0; i < amountOfColumns; i++) {
+			ColumnConstraints columnConstraints = new ColumnConstraints();
+			columnConstraints.setPercentWidth(100 / amountOfColumns);
+			this.view.getColumnConstraints().add(columnConstraints);
+		}
+		for (int j = 0; j < amountOfRows; j++) {
+			RowConstraints rowConstraints = new RowConstraints();
+			rowConstraints.setPercentHeight(100 / amountOfRows);
+			this.view.getRowConstraints().add(rowConstraints);
+		}
+	}
 }
