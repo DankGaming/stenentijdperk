a9169fe22e5033ce57d0ea3509f04a55e106660d
Juan Tombal <juantombal@live.nl>
2020-06-11 13:05:00 +0200

    tableauView

--- src/main/java/hsleiden/stenentijdperk/stenentijdperk/Models/TableauModel.java
+++ src/main/java/hsleiden/stenentijdperk/stenentijdperk/Models/TableauModel.java
@@ -3,7 +3,10 @@ package hsleiden.stenentijdperk.stenentijdperk.Models;
 
 import hsleiden.stenentijdperk.stenentijdperk.Helpers.Stamlid;
 import hsleiden.stenentijdperk.stenentijdperk.observers.LobbyObserver;
+import hsleiden.stenentijdperk.stenentijdperk.observers.TableauObservable;
+import hsleiden.stenentijdperk.stenentijdperk.observers.TableauObserver;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -12,6 +15,7 @@ public class TableauModel {
     private PlayerModel player;
     private int[] gereedschap;
     private boolean[] gereedschapGebruikt;
+    ArrayList<TableauObserver> observers = new ArrayList<>();
 
     public TableauModel(){
         stamleden = new LinkedList<>();
@@ -36,6 +40,9 @@ public class TableauModel {
     }
 
     public void notifyAllObservers() {
+        for(TableauObserver lo : observers) {
+            lo.update((TableauObservable) this);
+        }
     }
 
     public int[] getGereedschap() {
@@ -50,6 +57,15 @@ public class TableauModel {
     }
 
     public void verhoogGereedschap() {
-
+        int index = gereedschap.length - 1, totaal = 0;
+            for (int i = gereedschap.length - 1; i >= 0; i--){
+                int aantal = gereedschap[i];
+                totaal += aantal;
+                if (gereedschap[index] >= aantal){
+                    index = i;
+                }
+            }
+            gereedschap[index]++;
+            notifyAllObservers();
     }
 }
