ca0e6ac25cbe6b5e7894a639efd951d7f931da11
Thijs <thijsvdh12@gmail.com>
2020-06-17 11:43:02 +0200

    removed ;

--- src/main/java/hsleiden/stenentijdperk/stenentijdperk/Controllers/PlayerController.java
+++ src/main/java/hsleiden/stenentijdperk/stenentijdperk/Controllers/PlayerController.java
@@ -2,8 +2,6 @@ package hsleiden.stenentijdperk.stenentijdperk.Controllers;
 
 import hsleiden.stenentijdperk.stenentijdperk.Models.PlayerModel;
 
-;
-
 public class PlayerController {
 	private PlayerModel playermodel;
 
