56eada59303ea46e926b32e8b872b42a83333aa2
Joost de Jager <joostdejager2002@outlook.com>
2020-06-22 10:53:54 +0200

    cleaned up loginview

--- src/main/java/hsleiden/stenentijdperk/stenentijdperk/Views/LoginView.java
+++ src/main/java/hsleiden/stenentijdperk/stenentijdperk/Views/LoginView.java
@@ -11,7 +11,6 @@ import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
