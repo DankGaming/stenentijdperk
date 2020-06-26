3c9bd8943dd3b5876b28a3ff4fa7a10c20c1aef2
Thijs <thijsvdh12@gmail.com>
2020-06-26 15:10:11 +0200

    clean

--- src/main/java/hsleiden/stenentijdperk/stenentijdperk/Helpers/Beschavingskaarten/Kaart.java
+++ src/main/java/hsleiden/stenentijdperk/stenentijdperk/Helpers/Beschavingskaarten/Kaart.java
@@ -4,48 +4,48 @@ import hsleiden.stenentijdperk.stenentijdperk.Helpers.Resource;
 import hsleiden.stenentijdperk.stenentijdperk.Interfaces.Status;
 
 public class Kaart implements Status {
-	//wat kost de kaart
-	private int kosten;
-	private Resource tool;
-	private Resource graan;
-	private String treasure;
-	private boolean status;
-	private String path;
+    //wat kost de kaart
+    private int kosten;
+    private Resource tool;
+    private Resource graan;
+    private String treasure;
+    private boolean status;
+    private final String path;
 
-	public Kaart(int kosten, String path) {
-		this.path = path;
-		this.kosten = kosten;
-		this.status = false;
-	}
+    public Kaart(int kosten, String path) {
+        this.path = path;
+        this.kosten = kosten;
+        this.status = false;
+    }
 
-	public String getPath() {
-		return this.path;
-	}
+    public String getPath() {
+        return this.path;
+    }
 
-	public int getPunten() {
-		return this.kosten;
-	}
+    public int getPunten() {
+        return this.kosten;
+    }
 
-	public Resource getTool() {
-		return this.tool;
-	}
+    public Resource getTool() {
+        return this.tool;
+    }
 
-	public Resource getGraan() {
-		return this.graan;
-	}
+    public Resource getGraan() {
+        return this.graan;
+    }
 
-	public String getTreasure() {
-		return this.treasure;
-	}
+    public String getTreasure() {
+        return this.treasure;
+    }
 
-	@Override
-	public boolean getStatus() {
-		return this.status;
-	}
+    @Override
+    public boolean getStatus() {
+        return this.status;
+    }
 
-	@Override
-	public void setStatus(boolean status) {
-		this.status = status;
-	}
+    @Override
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 
 }
