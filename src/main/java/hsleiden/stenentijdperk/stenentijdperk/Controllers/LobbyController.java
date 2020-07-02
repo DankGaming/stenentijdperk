fa174413164b6f6f39e8105e0ec2baecaf8ba360
larsverhoorn <s1117923@student.hsleiden.nl>
2020-07-03 00:16:49 +0200

    fixed being able to join lobby multiple times

--- src/main/java/hsleiden/stenentijdperk/stenentijdperk/Controllers/LobbyController.java
+++ src/main/java/hsleiden/stenentijdperk/stenentijdperk/Controllers/LobbyController.java
@@ -25,7 +25,7 @@ public class LobbyController {
 
     public void joinLobby() {
         if (this.lobbyId != 0)
-            if (FirebaseController.getAmountofPlayersInLobby(this.lobbyId) < 4 || !FirebaseController.getGamestatus(this.lobbyId) ) {
+            if (FirebaseController.getAmountofPlayersInLobby(this.lobbyId) < 4 && !FirebaseController.getGamestatus(this.lobbyId) && !FirebaseController.getPlayers().contains(p)) {
                 this.p.setPlayerNumber(setPlayerNumber());
                 if (FirebaseController.getAmountofPlayersInLobby(this.lobbyId) == 0) {
                     FirebaseController.setLobbyLeader(this.lobbyId, this.p);
